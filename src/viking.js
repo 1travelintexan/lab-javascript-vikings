f835129d76ac247064b06cecff3242905702743f
Joshua George <horheyinc8@gmail.com>
2021-03-29 20:41:10 +0200

    the best I can do for now on the Viking project

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,90 @@
+
 // Soldier
-class Soldier {}
+class Soldier {
+    
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+
+    attack(){
+    return this.strength
+    }
+    
+    receiveDamage(damage){
+         this.health = this.health - damage
+        }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+    }   
+
+attack(){
+    return this.strength
+    }
+
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health > 0){
+        return `${this.name} has received ${damage} points of damage`
+        }else{
+        return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+        super(health, strength)
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health = this.health- damage
+    
+
+    if(this.health > 0){
+        return `A Saxon has received ${damage} points of damage`
+    } else {
+        return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+
+class War {
+    constructor(){
+    }
+    vikingArmy = []
+    saxonArmy = []
+
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack(){
+        let saxon1 = Math.floor(Math.random() * this.saxonArmy.length) 
+        let viking1 = Math.floor(Math.random() * this.vikingArmy.length)
+        let newHealth = this.saxonArmy[Saxon1].health = this.saxonArmy[Saxon1].health - this.vikingArmy.strength
+        if(this.saxonArmy[saxon1].health < 0){
+            console.log('A Saxon has died in combat')
+            return this.saxonArmy.splice(this.saxonArmy,[saxon1])
+        }
+    
+    }
+
+}
